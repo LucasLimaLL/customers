acc629ee2e649f1bf5379850a2f1f8fc57cb27db
lucaslimall <git@git.com>
2023-02-13 16:38:53 -0300

    start project

---
+++ src/test/java/com/lucaslima/cms/CmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucaslima.cms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
